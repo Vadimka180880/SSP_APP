42daf502e3d5d4146b9a39f62ea3fa26b5f10d01
Vadimka180880 <vadim.te180880@gmail.com>
2025-05-10 21:16:24 -0400

    � Fix port binding for Render deployment

--- app.py
+++ app.py
@@ -90,4 +90,5 @@ def logout():
 
 # 🔁 Запуск сервера
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
